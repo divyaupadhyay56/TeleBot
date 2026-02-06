b6a8f3d1cc76e98cf8c9fbb62b7f8fda49c70ddb
Nishant Jain <jainnikhil0708@gmail.com>
2026-02-06 13:26:36 +0530

    registration, login and log out is working properly

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,73 +1,69 @@
 from flask import Blueprint, request
-from app.services.auth_services import login_user, register_user, authenticate_user, logout_user
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, get_jwt_identity, get_jti, decode_token)
+from app.services.auth_services import register_user, authenticate_user, logout_user
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+    decode_token,
+)
 from datetime import datetime, timedelta
 from app.database.db import db
-from app.database.models import User, Role, UserRole,OTP, RefreshToken
-from app.utils.password_utils import verify_password
+from app.database.models import User, Role, UserRole, OTP, RefreshToken
 
 
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-
-auth_bp = Blueprint('auth',__name__, url_prefix='/auth')
-
-@auth_bp.route('/register', methods =['POST'])
+@auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.json
-    phone = data['phone']
-    password = data['password']
-    role_name = data['role_name']
+    phone = data["phone"]
+    password = data["password"]
+    role_name = data["role_name"]
 
     if not phone or not password or not role_name:
-        return {"error":"Missing required fields"}, 400
-    return register_user(
-        phone ,
-       password,
-       role_name
-    )
+        return {"error": "Missing required fields"}, 400
+    return register_user(phone, password, role_name)
 
 
-@auth_bp.route('/login', methods = ["POST"])
+@auth_bp.route("/login", methods=["POST"])
 def login():
     data = request.json
-    phone = data.get('phone')
-    password = data.get('password')
+    phone = data.get("phone")
+    password = data.get("password")
     if isinstance(phone, (list, tuple)):
         phone = phone[0]
     # print(type(phone), phone)
     if not phone or not password:
-        return {"error":"Missing credentials"}, 400
-    
+        return {"error": "Missing credentials"}, 400
+
     user = authenticate_user(phone, password)
     if not user:
-        return {"error":"Invalid credentials"}, 401
-    
+        return {"error": "Invalid credentials"}, 401
+
     access_token = create_access_token(identity=str(user.id))
     refresh_token = create_refresh_token(identity=str(user.id))
-    decoded = decode_token(refresh_token)    
+    decoded = decode_token(refresh_token)
     # store fresh token
     db.session.add(
         RefreshToken(
-            user_id = user.id,
-            token = decoded['jti'],
-            expires_at = datetime.utcnow() + timedelta(days=7)
+            user_id=user.id,
+            token=decoded["jti"],
+            expires_at=datetime.utcnow() + timedelta(days=7),
         )
     )
     db.session.commit()
-    
-    return {
-        "access_token": access_token,
-        "refresh_token": refresh_token
-    }, 200
 
-@auth_bp.route("/refresh", methods= ["POST"])
-@jwt_required(refresh = True)
+    return {"access_token": access_token, "refresh_token": refresh_token}, 200
+
+
+@auth_bp.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
 def refresh():
     user_id = int(get_jwt_identity())
     access_token = create_access_token(identity=user_id)
     return {"access_token": access_token}, 200
-
 
 
 @auth_bp.route("/logout", methods=["POST"])
@@ -103,6 +99,7 @@ def request_otp():
     # send SMS/email here
     return {"message": "OTP sent"}, 200
 
+
 @auth_bp.route("/verify-otp", methods=["POST"])
 def verify_otp():
     data = request.json
@@ -111,11 +108,7 @@ def verify_otp():
     code = data.get("code")
     purpose = data.get("purpose")
 
-    otp = OTP.query.filter_by(
-        email=contact,
-        code=code,
-        purpose=purpose
-    ).first()
+    otp = OTP.query.filter_by(email=contact, code=code, purpose=purpose).first()
 
     if not otp or otp.expires_at < datetime.utcnow():
         return {"error": "Invalid or expired OTP"}, 400
--- backend/app/services/auth_services.py
+++ backend/app/services/auth_services.py
@@ -2,7 +2,7 @@ from app.database.models import User, Role, UserRole, RefreshToken
 from app.database.db import db
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.database.models import SystemRole
-from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, get_jwt
+from flask_jwt_extended import  get_jwt_identity
 
 
 def register_user(phone, password, role_name):
@@ -36,34 +36,34 @@ def authenticate_user(phone, password):
         return None
     return user
 
-def login_user(phone, password):
-    user = User.query.filter_by(phone=phone, is_active=True).first()
-    if not user:
-        return {"error": "Invalid credentials"}, 401
+# def login_user(phone, password):
+#     user = User.query.filter_by(phone=phone, is_active=True).first()
+#     if not user:
+#         return {"error": "Invalid credentials"}, 401
 
-    if not check_password_hash(user.password_hash, password):
-        return {"error": "Invalid credentials"}, 401
+#     if not check_password_hash(user.password_hash, password):
+#         return {"error": "Invalid credentials"}, 401
 
-    access_token = create_access_token(identity=user.id)
-    refresh_token = create_refresh_token(identity=user.id)
+#     access_token = create_access_token(identity=user.id)
+#     refresh_token = create_refresh_token(identity=user.id)
 
-    # print("uSER LOGIN SUCCESSFULL")
-    return {
-        "access_token": access_token,
-        "refresh_token": refresh_token
-    }, 200
+#     # print("uSER LOGIN SUCCESSFULL")
+#     return {
+#         "access_token": access_token,
+#         "refresh_token": refresh_token
+#     }, 200
 
 def logout_user():
     """
     Revoke the refresh token used for logout
     """
     user_id = int(get_jwt_identity())
-    jti = get_jwt()["jti"]
+    # jti = get_jwt()["jti"]
 
     token = RefreshToken.query.filter_by(
-        user_id=user_id,
-        token=jti,
-        revoked=False
+        user_id=user_id
+        # token=jti,
+        # revoked=False
     ).first()
 
     if not token:
